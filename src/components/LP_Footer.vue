bfe449f54cd6380356bbaa1ddca44ff858a000ad
JeremieN6 <ngoyi.jeremie@gmail.com>
2025-05-27 11:50:42 +0200

    Changement de l'url vers la page d'accueil ou le site du développeur

--- src/components/LP_Footer.vue
+++ src/components/LP_Footer.vue
@@ -110,17 +110,17 @@
     <div class="mx-auto max-w-screen-xl">
         <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
-                <a href="https://flowbite.com" class="flex items-center">
+                 <RouterLinker to="/" class="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-8" alt="FlowBite Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                </a>
+                </RouterLinker
             </div>
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                 <div>
                     <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                     <ul class="text-gray-600 dark:text-gray-400">
                         <li class="mb-4">
-                            <a href="https://flowbite.com" class="hover:underline">Flowbite</a>
+                             <RouterLinker to="/" class="hover:underline">Flowbite</RouterLinker
                         </li>
                         <li>
                             <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
@@ -153,7 +153,7 @@
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://jeremiecode.fr" class="hover:underline">Flowbite™</a>. All Rights Reserved.
             </span>
             <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                 <a class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
--- src/components/LP_Header.vue
+++ src/components/LP_Header.vue
@@ -3,11 +3,11 @@
         <header>
       <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-              <a href="https://flowbite.com" class="flex items-center">
+              <RouterLinker to="/" class="flex items-center">
                   <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9"
                       alt="AirFryTime Logo" />
                   <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">AirFryTime 🍗</span>
-              </a>
+              </RouterLinker>
               <div class="flex items-center lg:order-2">
               <RouterLink to="/#"
                   class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
