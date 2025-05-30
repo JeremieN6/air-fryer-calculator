53a8a264974fb01ef6fd0909d9a98e856fbb40ab
JeremieN6 <ngoyi.jeremie@gmail.com>
2025-05-30 11:07:08 +0200

    Ajout du fichier _headers pour définir le type de contenu de sitemap.xml

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -11,28 +11,41 @@ const __dirname = path.dirname(__filename)
 
 const BASE_URL = 'https://temps-cuisson-air-fryer.netlify.app'
 
+// 📅 Date du jour au format YYYY-MM-DD
+const today = new Date().toISOString().split('T')[0]
+
 const routes = [
   '/',
   '/calculateur-cuisson-air-fryer',
   '/fonctionnalites',
   '/blog',
   '/politique-de-confidentialite'
-]
+].map(path => ({ path, lastmod: today }))
 
-const blogRoutes = posts.map(post => `/blog/${post.slug}`)
+// 📝 Articles du blog : extraire la date et la transformer en format ISO
+const blogRoutes = posts.map(post => {
+  const rawDate = post.date // ex: "27 Mai 2025"
+  const parsedDate = new Date(rawDate)
+  const isoDate = isNaN(parsedDate) ? today : parsedDate.toISOString().split('T')[0]
+  return {
+    path: `/blog/${post.slug}`,
+    lastmod: isoDate
+  }
+})
 
+// 🧩 Fusionner tous les chemins
 const allRoutes = [...routes, ...blogRoutes]
 
 const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-${allRoutes.map(route => `
+${allRoutes.map(({ path, lastmod }) => `
   <url>
-    <loc>${BASE_URL}${route}</loc>
+    <loc>${BASE_URL}${path}</loc>
+    <lastmod>${lastmod}</lastmod>
     <changefreq>weekly</changefreq>
-    <priority>${route === '/' ? '1.0' : '0.8'}</priority>
+    <priority>${path === '/' ? '1.0' : '0.8'}</priority>
   </url>`).join('\n')}
 </urlset>
 `
-
 fs.writeFileSync(path.resolve(__dirname, 'public/sitemap.xml'), xml)
 console.log('✅ sitemap.xml généré avec succès')
