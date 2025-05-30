e0f3ed1a0c1813ceede34aef46ad0bf1dfd52fad
Jeremie <ngoyi.jeremie@gmail.com>
2025-05-30 14:15:58 +0200

    Fix : Mobile
    Ajustement de la largeur des éléments de résultat dans le composant cuissonForm.vue

--- src/components/cuissonForm.vue
+++ src/components/cuissonForm.vue
@@ -68,7 +68,7 @@
 <div v-if="resultatVisible" class="mt-10">
   <h2 class="text-2xl font-bold mb-4">Résultat</h2>
   <div class="flex flex-col md:flex-row justify-center items-start md:items-center my-5 gap-5">
-    <div class="block w-1/3 p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div class="block w-full md:w-1/3 p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <p class="font-normal text-gray-700 dark:text-gray-400">Aliment</p>
       <div class="flex items-center justify-between">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ resultat.aliment }}</h5>
@@ -78,7 +78,7 @@
         </svg>
       </div>
     </div>
-    <div class="block w-1/3 p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div class="block w-full md:w-1/3 p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <p class="font-normal text-gray-700 dark:text-gray-400">Temps de cuisson</p>
       <div class="flex items-center justify-between">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ resultat.temps }}</h5>
@@ -88,7 +88,7 @@
       </div>
      
     </div>
-    <div class="block w-1/3 p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div class="block w-full md:w-1/3 p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <p class="font-normal text-gray-700 dark:text-gray-400">Température</p>
       <div class="flex items-center justify-between">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ resultat.temperature }}</h5>
