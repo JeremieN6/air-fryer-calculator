095d861e93f7439900203bb0ce6a134746a064aa
JeremieN6 <ngoyi.jeremie@gmail.com>
2025-06-02 12:16:04 +0200

    refactor: Simplification de la configuration de vite.config.js en supprimant le chargement dynamique des variables d'environnement et mise à jour de .gitignore (Ne fonctionne pas)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
-
-  return {
-    plugins: [vue()],
-    base: env.VITE_APP_BASE_URL, // ← dynamique selon le fichier .env
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
 })
