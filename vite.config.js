bec9610e0f1b91651f324c5284c00f455a4bff83
JeremieN6 <ngoyi.jeremie@gmail.com>
2025-06-02 11:59:32 +0200

    Ajout de la configuration de base dynamique dans vite.config.js pour gérer les chemins en mode production et développement.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base:  mode === 'production' ? '/temps-cuisson-air-fryer/' : '/', // ← clé du problème
   plugins: [vue()],
-})
+}))
