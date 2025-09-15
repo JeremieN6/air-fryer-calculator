140d13b66d0edcd0e99dfce77d4263ace42b087f
Jeremie <ngoyi.jeremie@gmail.com>
2025-09-15 22:14:46 +0200

    feat: add LP_Header and LP_Footer components to PolitiqueConfidentialitePage

--- src/pages/PolitiqueConfidentialitePage.vue
+++ src/pages/PolitiqueConfidentialitePage.vue
@@ -1,9 +1,13 @@
 <template>
     <section>
+      <LP_Header />
       <PolitiqueConfidentialite />
+      <LP_Footer />
     </section>
   </template>
 
 <script setup>
+    import LP_Header from '../components/LP_Header.vue'
     import PolitiqueConfidentialite from '../components/PolitiqueConfidentialite.vue'
+    import LP_Footer from '../components/LP_Footer.vue'
 </script>
